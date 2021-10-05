2154ca52de877dbd9a759180d4101b840815afb3
oquendito <86526751+oquendito@users.noreply.github.com>
2021-10-05 14:48:15 -0300

    pre_entrega_js

--- app_2_.js
+++ app_2_.js
@@ -1,10 +1,10 @@
-let afirmacion = prompt("Desea ingresar al sitio?");
+/*let afirmacion = prompt("Desea ingresar al sitio?");
 
 if(afirmacion ="Si"){
         alert("Inicie sesión");
 }
 
-/*function login(){
+function login(){
   let form = document.form;
   let user = form.usuario.value;
   let pass = form.contraseña.value;
@@ -24,7 +24,7 @@ let user = {
   }
 };
 
-console.log(user);*/
+console.log(user);
 
 class User {
   constructor(name,email,pass) {
@@ -45,4 +45,46 @@ if(user.login(prompt("ingresa la clave"))){
   console.log('Login valido');
 }else{
   console.log('clave incorrecta');
+}*/
+
+function getDB(){
+  try{
+      if(typeof window.baseDatos == typeof undefined || window.baseDatos == null){
+          window.baseDatos = [];
+      }   
+      return window.baseDatos;
+  }catch(error){
+      console.error(error);
+  }
 }
+
+class Persona {
+  constructor(usuario, password) {
+      this.usuario  = usuario;
+      this.password = password;
+  }
+  almacenar(){
+      try{
+          let db = getDB(),
+              amount = db.length;
+          return amount < (this.agregar(new Persona(this.usuario,this.password))).length;
+      }catch(error){
+          console.error(error);
+      }
+  }
+  agregar(persona){
+      (getDB()).push(persona);
+      return baseDatos;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  document.getElementById('button').addEventListener("click", function(event) { 
+      let user = new Persona(document.getElementById("usuario").value, document.getElementById("password").value);
+      if(user.almacenar()){
+          alert(`Usuario guardado correctamente. Cantidad de usuarios registrados: ${(getDB()).length}`);
+      }else{
+          console.error('No se guardo');
+      }
+  });
+});
