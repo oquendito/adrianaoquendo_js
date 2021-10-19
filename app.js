5b5f7ba7c38a83281490f0c8654f351e5abbec23
oquendito <86526751+oquendito@users.noreply.github.com>
2021-10-19 15:02:54 -0300

    entrega_11_js

--- app.js
+++ app.js
@@ -63,16 +63,10 @@ function generarTabla(numero){
     }
 }
 
-*/
 
-const productos = [
-    {nombre: 'Gatabis', precio: 2 },
-    {nombre: 'Libro para colorear', precio: 4},
-    {nombre: 'Campera', precio: 8},
-    {nombre: 'Collar', precio: 2},
-    {nombre: 'Rascador reciclado', precio: 6},
-    {nombre: 'Arena para gatos', precio: 8}
-];
+
+
+
 
 const menores = productos.filter(p => p.precio <=4 );
 
@@ -84,4 +78,30 @@ console.log(mayores);
 function saludar(){console.log (`Hola Seba! Hice un tipo de filtrado para el catalogo :)`);
 }
 
-saludar ();
+saludar ();*/
+
+
+
+const productos = [
+    {nombre: 'Gatabis', precio: 2 },
+    {nombre: 'Libro para colorear', precio: 4},
+    {nombre: 'Campera', precio: 8},
+    {nombre: 'Collar', precio: 2},
+    {nombre: 'Rascador reciclado', precio: 6},
+    {nombre: 'Arena para gatos', precio: 8}
+];
+
+console.log(JSON.stringify(productos));
+
+localStorage.setItem('producto', 'Libro para colorear');
+localStorage.setItem('precio', 4);
+
+let producto = localStorage.getItem('producto');
+let precio = localStorage.getItem('precio');
+
+console.log(producto, precio)
+
+localStorage.setItem('productosList', JSON.stringify(productos));
+
+const storedProductos = localStorage.getItem('productosList');
+console.log(JSON.parse(storedProductos))
