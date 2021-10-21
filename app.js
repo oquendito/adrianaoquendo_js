699b251bfc2b1e2aaed89c388b585aa1d4b0e8cb
oquendito <86526751+oquendito@users.noreply.github.com>
2021-10-21 13:17:55 -0300

    entrega_js

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-/*let afirmacion = prompt("Desea ingresar al sitio?");
-
-if(afirmacion ="Si"){
-        alert("Inicie sesión");
-}
-
-function login(){
-  let form = document.form;
-  let user = form.usuario.value;
-  let pass = form.contraseña.value;
-
-  if(user == "Seba" && pass == "coderhouse"){
-    alert("bienvenido Seba!");
-  }else{
-    alert("datos incorrectos");
-  }
-} 
- 
-let user = {
-  name: "Seba",
-  location: "Argentina",
-  login: function() {
-    console.log("the user logged in! :)");
-  }
-};
-
+//inicio de sesion de usuario
 console.log(user);
 
 class User {
@@ -45,7 +20,8 @@ if(user.login(prompt("ingresa la clave"))){
   console.log('Login valido');
 }else{
   console.log('clave incorrecta');
-}*/
+}
+
 
 function userNameEntered() {
   var user = document.forms["user"]["name"].value;
@@ -89,52 +65,17 @@ class Persona {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  document.getElementById('button').addEventListener("click", function(event) { 
+document.addEventListener("DOMContentLoaded", function() { 
+  document.getElementById('button').addEventListener("click", function() { 
       let user = new Persona(document.getElementById("usuario").value, document.getElementById("password").value);
       if(user.almacenar()){
           alert(`Usuario guardado correctamente. Cantidad de usuarios registrados: ${(getDB()).length}`);
       }else{
           console.error('No se guardo');
       }
-  });
-});
 
 localStorage.setItem('usuario', 'password');
 localStorage.setItem('usuario', JSON.stringify(this.usuario));
 
-/*let arrayUsuarios = [];
-let formulario = document.querySelector("#formulario")
-let inputUsuario = document.querySelector("#iUsuario");
-
-let usuarioI = formulario.children[1].value;
-let passwordI = formulario.children[5].value;
-
-let contenedor = document.querySelector("#usuarioIngresado");
-let mostrarTodos = document.querySelector("#mostrarTodos");
-let parrafo = mostrarTodos.getElementsByTagName("p");
-let bandera = false;
-
-formulario.addEventListener("submit" , agregarUsuario);
-btnMostrar.addEventListener("click" , MostrarUsuarios);
-
-inputUsuario.focus();
-
-function MostrarUsuarios(e) {
-        e.preventDefault();
-        let i = 0;
-        mostrarTodos.innerHTML = '<h3< Lista de Usuarios:</h3>';
-        for (const usuario of arrayUsuarios){
-        mostrarTodos.innerHTML += `
-      <p><strong> Usuario:</strong> ${usuarioI}</p>
-      <p><strong> Password: </strong> ${passwordI}</p>
-      <hr>`;
-        }
-      }
-      function agregarAlDom() {
-    contenedor.innerHTML = `<h3>Ultimo inicio de sesion</h3>
-  <p><strong> Usuario:</strong> ${usuarioI}</p>
-  <p><strong> Password: </strong> ${passwordI}</p>
-  <hr>`;
-}*/
-
+  });
+});
